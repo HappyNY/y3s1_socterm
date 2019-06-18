f2c3bd9af93abe9847e727e330fabf8c52d9a2a3
Stephen Kang <ki6080@gmail.com>
2019-06-18 13:41:29 +0900

    Change priority of register bit occupying logic

--- project/resource/HDL/GPPCU/GPPCU_STALL_GEN.v
+++ project/resource/HDL/GPPCU/GPPCU_STALL_GEN.v
@@ -18,42 +18,50 @@ module GPPCU_STALL_GEN #
     iWRREG_VALID
 );
     `include "bit_fit.vh"
-    localparam ABW = bit_fit(NUMREG);
+    localparam RBW = bit_fit(NUMREG-1);
     
-    // ports
+    // -- ports
     input                   iACLK;
     input                   inRST;
     input                   iREGD;
-    input   [ABW-1:0]       iREGA;
-    input   [ABW-1:0]       iREGB;
+    input   [RBW-1:0]       iREGA;
+    input   [RBW-1:0]       iREGB;
     input                   iVALID_REGD;
     input                   iVALID_REGA;
     input                   iVALID_REGB;
     output                  oENABLED;
     
-    input   [ABW-1:0]       iWRREG;
+    input   [RBW-1:0]       iWRREG;
     input                   iWRREG_VALID;
     
-    // regs
+    // -- regs
     reg     [NUMREG-1:0]    occupied;
+    wire    [NUMREG-1:0]    pending_occupy;
     wire    [NUMREG-1:0]    rq_mask_a, rq_mask_b; 
+    wire    [NUMREG-1:0]    wr_mask;
+
+    // -- logics
+    // wr_mask enables synchronous access to written reg on it. ... @defered.
+    assign oENABLED = !(|(occupied & ((rq_mask_a | rq_mask_b)/* & ~wr_mask*/)));
     
-    // logics
-    assign oENABLED = !(|(occupied & (rq_mask_a | rq_mask_b)));
+    // -- occupy logic
     generate 
         genvar i;
         for(i = 0; i < NUMREG; i = i + 1) begin : SELECT
             wire outsig = ~iWRREG_VALID | (i == iWRREG);
             wire insig  = oENABLED && iVALID_REGD && i == iREGD; 
+            assign pending_occupy[i] = (occupied[i] | insig) & outsig;
+            
             always @(posedge iACLK) begin
                 if(~inRST)
                     occupied[i] = 0;
                 else
-                    occupied[i] = (occupied[i] & outsig) | insig; 
+                    occupied[i] = pending_occupy[i]; 
             end
             
             assign rq_mask_a[i] = (i == iREGA) & iVALID_REGA;
             assign rq_mask_b[i] = (i == iREGB) & iVALID_REGB;
+            assign wr_mask[i]   = (i ==iWRREG) & iWRREG_VALID;
         end
     endgenerate
 endmodule
